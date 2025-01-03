134376b41c2ff3c7e344c07f3e1326fadab11fbb
Ragulandiran <145469214+Ragu162004@users.noreply.github.com>
2025-01-03 19:22:28 +0530

    Time: 1 ms (63.25%), Space: 41.2 MB (38.6%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if(num == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        int flag = 0;
+        if(num < 0) {
+            num = -1 * num;
+            flag = 1;
+        }
+        while(num != 0) {
+            sb.append(String.valueOf(num%7)); 
+            num = num / 7;
+        }
+        String s = flag == 1 ? sb.append('-').reverse().toString() : sb.reverse().toString();
+        return s;
+    }
+}
