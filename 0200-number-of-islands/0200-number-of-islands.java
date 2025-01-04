039ac8c0459a31c217bb44ee9285c4147fa8d833
Ragulandiran <145469214+Ragu162004@users.noreply.github.com>
2025-01-04 19:48:22 +0530

    Time: 2 ms (99.82%), Space: 49.8 MB (36.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,19 +1,14 @@
 class Solution {
     private void travelHelper(char[][] grid, int i ,int j) {
-        if(grid[i][j] == '0') return;
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return;
         grid[i][j] = '0';
-        if(i > 0)
-            travelHelper(grid, i - 1, j);
-        if(i < grid.length - 1)
-            travelHelper(grid, i + 1, j);
-        if(j > 0)
-            travelHelper(grid, i, j - 1);
-        if(j < grid[i].length - 1)
-            travelHelper(grid, i, j + 1);
+        travelHelper(grid, i - 1, j);
+        travelHelper(grid, i + 1, j);
+        travelHelper(grid, i, j - 1);
+        travelHelper(grid, i, j + 1);
     }
     public int numIslands(char[][] grid) {
         int count = 0;
-
         for(int i = 0; i < grid.length; i++) {
             for(int j = 0; j < grid[i].length; j++) {
                 if(grid[i][j] == '1') {
