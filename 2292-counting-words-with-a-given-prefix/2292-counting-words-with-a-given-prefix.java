85fc7dac4d80213c4cafac2b6a7e467076cc66d2
Ragulandiran <145469214+Ragu162004@users.noreply.github.com>
2025-01-09 20:05:06 +0530

    Time: 4 ms (10.57%), Space: 46.1 MB (5.1%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
@@ -1,9 +1,43 @@
 class Solution {
-    public int prefixCount(String[] words, String pref) {
-        int res = 0;
-        for(int i = 0; i < words.length; i++) {
-            if(words[i].startsWith(pref)) res++;
+
+    class TrieNode {
+        TrieNode[] child;
+        int count;
+        TrieNode(){
+            child = new TrieNode[26];
+            count = 0;
         }
-        return res;
+    }
+
+    private void insert(String s, TrieNode root) {
+        TrieNode curr = root;
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+            if (curr.child[idx] == null) {
+                curr.child[idx] = new TrieNode();
+            }
+            curr = curr.child[idx];
+            curr.count++;
+        }
+    }
+
+    private int helper(String s, TrieNode root) {
+        TrieNode curr = root;
+        for (char c : s.toCharArray()) {
+            int idx = c - 'a';
+            if (curr.child[idx] == null) {
+                return 0; 
+            }
+            curr = curr.child[idx];
+        }
+        return curr.count;
+    }
+
+    public int prefixCount(String[] words, String pref) {
+        TrieNode root = new TrieNode();
+        for (String word : words) {
+            insert(word, root);
+        }
+        return helper(pref, root);
     }
 }
