63271ad0c4de0b51c56bd5c1d49352fe8376ebdd
Ragulandiran <145469214+Ragu162004@users.noreply.github.com>
2025-12-01 09:22:59 +0530

    Time: 0 ms (100%), Space: 61.2 MB (29.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,5 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        if(nums.length <= 1) return;
         if (nums.length!=0) k=k%nums.length;
         reverse(nums , 0 , nums.length-1);
         reverse(nums, 0, k-1);
